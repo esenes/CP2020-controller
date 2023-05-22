adeda39a36d82360bba4e9aae6998a11c4888895
Eugenio Senes <esenes@users.noreply.github.com>
2023-05-22 16:15:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(name='CP2020-controller',
     packages=find_packages(),
     classifiers=CLASSIFIERS,
     install_requires=REQUIREMENTS,
-    dependency_links=['git+git://github.com/nelsond/prologix-gpib-ethernet.git'],
+    dependency_links=['git+https://github.com/nelsond/prologix-gpib-ethernet.git'],
     keywords='GPIB flann attenuator phase-shifter wrapper',
 )
